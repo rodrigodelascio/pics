31b28befdb88de7ec39b8cf1b8ae5c04a0164bf2
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-27 12:37:44 +0100

    final app

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,7 +17,7 @@ function SearchBar({ onSubmit }) {
   return (
     <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
-        <label>Enter Search Term</label>
+        <label>Search images:</label>
         <input value={term} onChange={handleChange} />
       </form>
     </div>
